0a96b44d20da2826d98abc734cf1fcaf8b644724
Onifade Joshua <onifadjosh@gmail.com>
2024-05-02 19:41:52 +0100

    Todo app

---
+++ lib/screens/home.dart
@@ -0,0 +1,144 @@
+import 'package:flutter/material.dart';
+import 'package:todo_app/constants/colors.dart';
+import '../widgets/todo_item.dart';
+import '../model/todo.dart';
+// import '../widgets/search_box.dart';
+
+
+final todoslist = ToDo.todoList();
+class Home extends StatelessWidget {
+  const Home({super.key});
+  
+
+  
+
+  @override
+  Widget build(BuildContext context) {
+    return  Scaffold(
+      backgroundColor: tdBGColor,
+      appBar: _buildAppBar(),
+      body: Stack(
+        children: [
+          Padding(
+            padding: EdgeInsets.symmetric(horizontal: 20, vertical: 15),
+            child: Column(
+              children: [
+                SearchBox(), 
+                Expanded(
+                  child: ListView(
+                    children: [
+                      Container(
+                        margin: EdgeInsets.only(top: 50),
+                        child: Text('All ToDos', style: TextStyle(fontSize:30, fontWeight: FontWeight.w500)),
+                      ),
+          
+                      for (ToDo todoo in todoslist) 
+                        TodoItem(todo: todoo),
+                      
+                      
+          
+                    ],
+                    
+                  ),
+                )
+              ],
+            ),
+          ),
+          Align(
+            alignment: Alignment.bottomCenter,
+            child: Row(
+              children: [
+                Expanded(child: Container(
+                  margin: EdgeInsets.only(
+                    bottom: 20,
+                    right: 20,
+                    left: 20,
+                  ),
+                  padding: EdgeInsets.symmetric(horizontal: 20, vertical: 5),
+                  decoration: BoxDecoration(
+                    color: Colors.white,
+                    boxShadow: [BoxShadow(
+                      color: Colors.grey,
+                      offset: Offset(0, 0),
+                      blurRadius: 10.0,
+                      spreadRadius: 0.0,
+                    )],
+                    borderRadius: BorderRadius.circular(10)
+                  ),
+                  child: TextField(
+                    decoration: InputDecoration(
+                      hintText: 'Add a new todo item',
+                      border: InputBorder.none
+                    ),
+                  )
+                )
+                  
+                )
+              ],
+            ),
+          )
+        ],
+      ),
+    );
+  }
+
+  AppBar _buildAppBar() {
+    return AppBar(
+      backgroundColor: tdBGColor,
+      elevation: 0,
+      title: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        crossAxisAlignment: CrossAxisAlignment.center,
+        children: [
+          const Icon(
+            Icons.menu,
+            color: tdBlack,
+            size: 30,
+          ),
+
+          Container(
+            width: 40,
+            height: 40,
+           
+            
+            child: ClipRRect(
+              borderRadius: BorderRadius.circular(20),
+              child: Image.asset('assets/images/avatar.jpeg'),
+            )
+            
+           
+          ),
+         
+        ],
+      ),
+  );
+  }
+}
+
+class SearchBox extends StatelessWidget {
+  const SearchBox({
+    super.key,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      decoration: BoxDecoration( color: Colors.white,
+        borderRadius: BorderRadius.circular(20)
+      ),
+      child:const Padding(
+        padding: EdgeInsets.symmetric(horizontal: 15),
+        child: TextField(
+          decoration: InputDecoration(
+            contentPadding: EdgeInsets.all(0),
+            prefixIcon: Icon(Icons.search, color: tdBlack, size: 20),
+            prefixIconConstraints: BoxConstraints(maxHeight: 25, maxWidth: 25),
+            border: InputBorder.none,
+            hintText: 'Search',
+            hintStyle: TextStyle(color: tdGrey)
+          ),
+        ),
+      ),
+    );
+  }
+}
